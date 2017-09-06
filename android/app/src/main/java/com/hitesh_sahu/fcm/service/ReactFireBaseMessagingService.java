334929dd97c3576b768d33ee51e4a6aae7e06a6a
Hitesh Sahu <hiteshkumarsahu1990@gmail.com>
2017-09-06 20:04:36 +0530

    Renamed File

---
+++ android/app/src/main/java/com/hitesh_sahu/fcm/service/ReactFireBaseMessagingService.java
@@ -0,0 +1,159 @@
+/*
+ *
+ *   Copyright (c) 2017. HiteshSahu.com- All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential.
+ *   Written by Hitesh Sahu <hiteshkrsahu@Gmail.com>, 2017.
+ *
+ *
+ */
+
+package com.hitesh_sahu.fcm.service;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.hitesh_sahu.reactnotification.MainActivity;
+import com.hitesh_sahu.utils.Config;
+import com.hitesh_sahu.utils.NotificationUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import me.leolin.shortcutbadger.ShortcutBadger;
+
+import static com.hitesh_sahu.utils.AppConstants.ENABLE_NOTIFICATION;
+import static com.hitesh_sahu.utils.PreferenceHelper.getPrefernceHelperInstace;
+
+
+public class ReactFireBaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = ReactFireBaseMessagingService.class.getSimpleName();
+
+    private NotificationUtils notificationUtils;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        //Check if Notn are Enabled?
+        if (getPrefernceHelperInstace().getBoolean(getApplicationContext(),
+                ENABLE_NOTIFICATION, true)) {
+
+            if (remoteMessage == null)
+                return;
+
+            ShortcutBadger.applyCount(getApplicationContext(), 1);
+
+            Log.e(TAG, "From: " + remoteMessage.getFrom());
+
+            // Check if message contains a notification payload.
+            if (remoteMessage.getNotification() != null) {
+                Log.e(TAG, "Notification Body: " + remoteMessage.getNotification().getBody());
+                handleNotification(remoteMessage.getNotification().getBody());
+            }
+
+            // Check if message contains a data payload.
+            if (remoteMessage.getData().size() > 0) {
+                Log.e(TAG, "Data Payload: " + remoteMessage.getData().toString());
+
+                try {
+                    JSONObject json = new JSONObject(remoteMessage.getData().toString());
+                    handleDataMessage(json);
+                } catch (Exception e) {
+                    Log.e(TAG, "Exception: " + e.getMessage());
+                }
+            }
+        } else {
+            Log.e(TAG, "ReactFireBaseMessagingService: Notifications Are Disabled by User");
+
+        }
+    }
+
+    private void handleNotification(String message) {
+        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+            // app is in foreground, broadcast the push message
+            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+            pushNotification.putExtra("message", message);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+
+            // play notification sound
+            NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
+            notificationUtils.playNotificationSound();
+        } else {
+            // If the app is in background, firebase itself handles the notification
+        }
+    }
+
+    private void handleDataMessage(JSONObject json) {
+        Log.e(TAG, "push json: " + json.toString());
+
+        try {
+            JSONObject data = json.getJSONObject("data");
+
+            String title = data.getString("title");
+            String message = data.getString("message");
+            boolean isBackground = data.getBoolean("is_background");
+            String imageUrl = data.getString("image");
+            String timestamp = data.getString("timestamp");
+            JSONObject payload = data.getJSONObject("payload");
+
+            Log.e(TAG, "title: " + title);
+            Log.e(TAG, "message: " + message);
+            Log.e(TAG, "isBackground: " + isBackground);
+            Log.e(TAG, "payload: " + payload.toString());
+            Log.e(TAG, "imageUrl: " + imageUrl);
+            Log.e(TAG, "timestamp: " + timestamp);
+
+
+            if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+                // app is in foreground, broadcast the push message
+                Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+                pushNotification.putExtra("message", message);
+                LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+
+                // play notification sound
+                NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
+                notificationUtils.playNotificationSound();
+            } else {
+                // app is in background, show the notification in notification tray
+                Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+                resultIntent.putExtra("message", message);
+
+                // check for image attachment
+                if (TextUtils.isEmpty(imageUrl)) {
+                    showNotificationMessage(getApplicationContext(), title, message, timestamp, resultIntent);
+                } else {
+                    // image is present, show notification with image
+                    showNotificationMessageWithBigImage(getApplicationContext(), title, message, timestamp, resultIntent, imageUrl);
+                }
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Json Exception: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Showing notification with text only
+     */
+    private void showNotificationMessage(Context context, String title, String message, String timeStamp, Intent intent) {
+        notificationUtils = new NotificationUtils(context);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        notificationUtils.showNotificationMessage(title, message, timeStamp, intent);
+    }
+
+    /**
+     * Showing notification with text and image
+     */
+    private void showNotificationMessageWithBigImage(Context context, String title, String message, String timeStamp, Intent intent, String imageUrl) {
+        notificationUtils = new NotificationUtils(context);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        notificationUtils.showNotificationMessage(title, message, timeStamp, intent, imageUrl);
+    }
+}
